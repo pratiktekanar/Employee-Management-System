17363077a337639fb213468ac93d593eabbbefaa
pratiktekanar <pratiktekanar5044@gmail.com>
2025-07-10 04:02:36 +0530

    Employee-Management-System

---
+++ src/main/java/com/Employee/Management/System/Employee/entity/Employee.java
@@ -0,0 +1,31 @@
+package com.Employee.Management.System.Employee.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "First_Name")
+    private String firstName;
+
+    @Column(name = "Last_Name")
+    private String lastName;
+
+    @Column(name = "Email_ID" , nullable = false , unique = true)
+    private String email;
+
+
+
+}
